0862d5fbcb99c2aad9ed8e34092c0391babd9643
jessicamayvt <jessicamay@vt.edu>
2024-09-30 19:51:47 -0400

    Added the typewriter effect and updated the CSS

--- src/pages/WaitingPage.js
+++ src/pages/WaitingPage.js
@@ -3,6 +3,7 @@ import { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 import {SocketContext} from '../App' 
+import maroonLogo from '../images/maroonlogo2.png'
 
 export const WaitingPage = () => {
   const {redirectToSwipe} = useContext(SocketContext)
@@ -28,10 +29,12 @@ export const WaitingPage = () => {
   }
   
   return(
-    <div>
-      <h1>Waiting for host to decide{ellipsis}</h1>
+    <div className="waiting-container">
+      <img src={maroonLogo} alt="Logo" className="waiting-name-image"/>
+      <div>
+        <h2>Waiting for host to decide{ellipsis}</h2>
+      </div>
     </div>
-    
   );
 
 }
