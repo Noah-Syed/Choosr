47c9d561c4720ff5c11bfc0579943ac485eaf561
jessicamayvt <jessicamay@vt.edu>
2024-09-30 19:17:39 -0400

    Updated the waiting page to have ellipses

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import {SocketContext} from '../App'
 import { useNavigate } from 'react-router-dom'
+import maroonLogo from '../images/maroonlogo2.png'
 
 export const SearchPage = () => {
   const [inputName, setInputName] = useState('');
@@ -24,19 +25,22 @@ export const SearchPage = () => {
   };
 
   return (
-    <form id="form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search..."
-        aria-label="Search through site content"
-        id="inputName"
-        value={inputName}
-        onChange={(e) => setInputName(e.target.value)}  // Update state on input change
-      />
-      <button type="submit">
+    <div className="search-bar-container">
+      <img src={maroonLogo} alt="Logo" className="name-image"/>
+      <form id="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          aria-label="Search through site content"
+          id="inputName"
+          value={inputName}
+          onChange={(e) => setInputName(e.target.value)}  // Update state on input change
+        />
+        <button type="submit">
           <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
-      </button>
-    </form>
+        </button>
+      </form>
+    </div>
   );
 };
 
---
+++ src/pages/WaitingPage.js
@@ -0,0 +1,37 @@
+import '../App.css';
+import { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
+import {SocketContext} from '../App' 
+
+export const WaitingPage = () => {
+  const {redirectToSwipe} = useContext(SocketContext)
+  const [ellipsis, setDots] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setDots((prevDots) => {
+            if (prevDots.length < 3) {
+                return prevDots + '.';
+            } else {
+                return '';
+            }
+        });
+    }, 500); 
+
+    return () => clearInterval(interval);
+}, []);
+
+  if(redirectToSwipe){
+    navigate('/swipe');
+  }
+  
+  return(
+    <div>
+      <h1>Waiting for host to decide{ellipsis}</h1>
+    </div>
+    
+  );
+
+}
