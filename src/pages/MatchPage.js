541fe92408f24eef1e007d3b8c7633f7e388c3ca
jessicamayvt <jessicamay@vt.edu>
2024-09-30 23:25:02 -0400

    Added youMatched page

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -4,15 +4,40 @@ import xLogo from '../images/CircleX.png'
 import choosrLogo from '../images/choosrlogo.png'
 import nameLogo from '../images/name.png'
 import check from '../images/check.png'
+import youMatched from '../images/youMatched.png'
 import { SocketContext } from '../App'
 
 export const MatchPage = () => {
-    const [matchedCard, setMatchedCard] = useState(null);
+    const [showCard, setShowCard] = useState(false);
+    const [showCheckmark, setShowCheckmark] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowCard(true);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        if(showCard) {
+            const checkmarkTimer = setTimeout(() => {
+                setShowCheckmark(true);
+        }, 4000);
+        return () => clearTimeout(checkmarkTimer);
+        }
+    }, [showCard]);
+
     return (
         <div className="App">
           <div className="content-container">
             <img draggable = "false"
             src={nameLogo} alt="App Logo" className="app-logo" />
+                {!showCard ? (
+                    <div className="flashing-image-container">
+                       <img src={youMatched} alt="Matched" className="flasing-image"/>
+                    </div> 
+                ) : (
                 <div className="card"> 
                 <div className="content-wrapper">
                     <img draggable = "false"
@@ -29,7 +54,8 @@ export const MatchPage = () => {
                 src={check} alt="checkmark" className="checkmark"/>
               </div>
             </div>
+            )}
           </div>
         </div> 
-      );
+    );
 }
